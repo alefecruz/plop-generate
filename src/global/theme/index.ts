6f432b044e97cb5777cd99dce546bea256cb5765
Álefe Cruz <126995086+alefecruz@users.noreply.github.com>
2023-11-06 07:31:58 -0300

    Initial commit

---
+++ src/global/theme/index.ts
@@ -0,0 +1,11 @@
+export * as themeMappers from './mappers';
+export {
+    type IColorTheme,
+    type IFontTheme,
+    type IMediaTheme,
+    type ISelectTheme,
+    type ISizeTheme,
+    type ISpacingTheme,
+} from './interfaces';
+
+export { selectTheme } from './select-theme';
